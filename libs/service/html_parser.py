d67ac3e2ce14cd1afbbb39d2e99b39a82ea170b4
ryosyntaxx <riodwi12174@gmail.com>
2023-12-05 18:25:46 +0700

    first commit

---
+++ libs/service/html_parser.py
@@ -0,0 +1,11 @@
+import math
+import requests
+import json
+import re
+from time import sleep
+from requests import Response
+from pyquery import PyQuery
+from libs.utils.parser import HtmlParser
+
+class Scrapper:
+    pass
