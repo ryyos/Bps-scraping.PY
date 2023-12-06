cebec9f91243df51f4c40840d082a60a5e2cfc1e
ryosyntaxx <riodwi12174@gmail.com>
2023-12-06 12:48:43 +0700

    add feature in  main

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from time import sleep
 from requests import Response
 from pyquery import PyQuery
 from libs.utils.parser import HtmlParser
-parser = Scrapper()
+# parser = Scrapper()
 # parser.execute("https://www.archive.bps.go.id/subject/2/komunikasi.html#subjekViewTab3")
-parser.execute("https://www.archive.bps.go.id/subject/7/energi.html#subjekViewTab3")
+# parser.execute("https://www.archive.bps.go.id/subject/7/energi.html#subjekViewTab3")
 
 class Main:
     def __init__(self) -> None:
@@ -15,9 +15,17 @@ class Main:
         pass
 
     def get_url(self, main_url):
+        urls = []
+        types = []
+
         response: Response = requests.get(url=main_url)
         html = PyQuery(response.text)
+        sideBar = html.find(selector='#sidebar')
+        for type in self.__paerser.ex(html=sideBar, selector='#accordion-daftar-subjek2 h2'):
+            types.append(type.text)
+            print(type)
         
+
         pass
 
     def ex(self, main_url):
